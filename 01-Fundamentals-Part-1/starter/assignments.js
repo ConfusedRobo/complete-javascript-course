2e9ccb8d5ebcfaf6928f9feac38b81ce8a5dd1cb
Rajat Kanti Biswas <74067540+ConfusedRobo@users.noreply.github.com>
2020-11-29 23:03:52 +0530

    First commit.

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,14 @@
+const country = "India";
+const continent = "Asia";
+let population = 1_384_660_352;
+console.log(country + "\n" + continent + "\n" + population);
+
+const isIsland = false;
+let language;
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);      //this is a comment
+
+language = "Hindi";
+console.log(language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+console.log(10 + 23 + 78);
+console.log("Rajat Biswas");
+console.log(61);
+
+let firstName = "Rajat Biswas";
+console.log("First name: " + firstName);
+console.log("First name: " + firstName);
+console.log("First name: " + firstName);
